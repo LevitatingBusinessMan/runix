a755b075b06721d4c7b1c2badc7589c049eead9d
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2023-10-03 10:29:20 +0200

    print PML4T

--- src/entry.rs
+++ src/entry.rs
@@ -88,6 +88,9 @@ pub extern fn runix(mbi_pointer: *const BootInformation) -> ! {
         let bootloader_name = mbi.bootloader_name().unwrap();    
         println!("Booted from: {}", &bootloader_name.to_str().unwrap());
     
+        let (PML4T, flags) = x86_64::registers::control::Cr3::read();
+        println!("PML4T at {:#x?}", PML4T);
+
         println!("Memory areas: ");
         let memory_map = mbi.memory_map().unwrap();
         for entry in &memory_map.entries {
