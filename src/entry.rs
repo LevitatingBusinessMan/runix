9060150760ebd0ced39a80cbb905fbb05ef7a32f
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2025-12-13 05:32:45 +0100

    src_mut_refs is stable

--- src/entry.rs
+++ src/entry.rs
@@ -1,6 +1,5 @@
 #![no_std]
 #![no_main]
-#![feature(const_mut_refs)]
 #![feature(ptr_metadata)]
 #![feature(abi_x86_interrupt)]
 #![feature(const_trait_impl)]
@@ -75,17 +74,17 @@ pub extern fn runix(mbi_pointer: *const BootInformation) -> ! {
     }
 
     if conf::CONFIG.get().unwrap().print_info {
-        let bootloader_name = mbi.bootloader_name().unwrap();    
+        let bootloader_name = mbi.bootloader_name().unwrap();
         println!("Booted from: {}", &bootloader_name.to_str().unwrap());
-      
+
         let elf = mbi.elf_symbols().next().expect("No ELF symbols");
         println!(
             "Kernel at: {:#x?} - {:#x?}",
             elf.sections().into_iter().skip(1).map(|s| s.addr).min().unwrap(),
             elf.sections().into_iter().map(|s| s.addr + s.size).max().unwrap()
         );
-    
-        println!("Multiboot at: {:#7x?} - {:#7x?}", mbi_pointer, mbi_pointer as *const () as usize + mbi.total_size as usize);  
+
+        println!("Multiboot at: {:#7x?} - {:#7x?}", mbi_pointer, mbi_pointer as *const () as usize + mbi.total_size as usize);
 
         // let (PML4T, flags) = x86_64::registers::control::Cr3::read();
         // println!("PML4T at {:#x?}", PML4T);
