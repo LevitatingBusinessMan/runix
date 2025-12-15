724ea09ce162b03df2bc3b17ad95d21bc2968ed6
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2025-12-15 07:35:29 +0100

    fix the basix hex font parser

--- src/fonts/hex.rs
+++ src/fonts/hex.rs
@@ -1,23 +1,26 @@
-pub const UNSCII_8: [[u8; 8]; 127] = parse::<8, 127>(include_str!("../static/fonts/unscii-8-fantasy.hex"));
+//! .HEX font library
+pub const UNSCII_8: [[u8; 8]; 128] = parse::<8, 128>(include_str!("../static/fonts/unscii-8.hex"));
+pub const UNSCII_FANTASY_8: [[u8; 8]; 128] = parse::<8, 128>(include_str!("../static/fonts/unscii-8-fantasy.hex"));
+pub const UNSCII_16: [[u8; 16]; 128] = parse::<16, 128>(include_str!("../static/fonts/unscii-16.hex"));
 
 /// parse a hex font
 pub const fn parse<const S: usize, const N: usize>(source: &str) -> [[u8; S]; N] {
     let bytes = source.as_bytes();
     let mut i = 0;
     
-    let line_length = 5 + S * 2;
+    let line_length = 7 + S * 2;
     
     let mut buf: [[u8; S]; N] = [[0; S]; N];
     
     while i < N {
-        let byte_index = i * line_length + 5;
+        let byte_index = i * line_length + 6;
         let mut j = 0;
         while j < S {
-            let first = bytes[byte_index+j];
-            let second = bytes[byte_index+j+1];
+            let first = bytes[byte_index+j*2];
+            let second = bytes[byte_index+j*2+1];
             let byte = (hex_value(first) << 4) | hex_value(second);
             buf[i][j] = byte;
-            j += 2;
+            j += 1;
         }
         i +=1;
     }
@@ -30,6 +33,6 @@ const fn hex_value(byte: u8) -> u8 {
         b'0'..=b'9' => byte - b'0',
         b'a'..=b'f' => byte - b'a' + 10,
         b'A'..=b'F' => byte - b'A' + 10,
-        _ => 0,
+        _ => panic!("invalid hex"),
     }
 }
