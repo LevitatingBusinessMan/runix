6b276d1805ac3d54615553663fed1ba1cf7fa6d6
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2025-12-17 11:28:13 +0100

    remove old commented code

--- src/main.rs
+++ src/main.rs
@@ -83,42 +83,6 @@ unsafe extern "C" fn runix() -> ! {
         
     kdebug::kdebug();
     
-    //vga::clear();
-
-    // let mbi = BootInformation::load(mbi_pointer);
-
-    // MBI.call_once(|| mbi);
-
-    // conf::parse(mbi.boot_command_line().expect("Could not get cmdline").to_str().unwrap());
-        
-    // for tag in mbi.tags() {
-    //     if let multiboot::Tag::Unknown(type_, data) = tag {
-    //         wprintln!(" Unknown multiboot tag: type {} size: {:#x}", type_, data.len());
-    //     }
-    // }
-
-    // if conf::CONFIG.get().unwrap().welcome {
-    //     vga::print_at(vga::BUFFER_WIDTH/2 - WELCOME_STRING.len()/2, 12, WELCOME_STRING.as_bytes(), Color::Black, Color::White);
-    // }
-
-    // if conf::CONFIG.get().unwrap().print_info {
-    //     let bootloader_name = mbi.bootloader_name().unwrap();
-    //     println!("Booted from: {}", &bootloader_name.to_str().unwrap());
-
-    //     let elf = mbi.elf_symbols().next().expect("No ELF symbols");
-    //     println!(
-    //         "Kernel at: {:#x?} - {:#x?}",
-    //         elf.sections().into_iter().skip(1).map(|s| s.addr).min().unwrap(),
-    //         elf.sections().into_iter().map(|s| s.addr + s.size).max().unwrap()
-    //     );
-
-    //     println!("Multiboot at: {:#7x?} - {:#7x?}", mbi_pointer, mbi_pointer as *const () as usize + mbi.total_size as usize);
-
-    //     // let (PML4T, flags) = x86_64::registers::control::Cr3::read();
-    //     // println!("PML4T at {:#x?}", PML4T);
-    // }
-
-    // allocator::init();
 }
 
 #[macro_export]
